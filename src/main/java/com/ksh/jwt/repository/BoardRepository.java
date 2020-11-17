f092ca9f9094d9361bfb526cc36b31e28ddfc277
성혁 강 <95line@naver.com>
2020-11-17 19:09:43 +0900

    수정

--- src/main/java/com/ksh/jwt/repository/BoardRepository.java
+++ src/main/java/com/ksh/jwt/repository/BoardRepository.java
@@ -2,6 +2,7 @@ package com.ksh.jwt.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -11,9 +12,7 @@ import com.ksh.jwt.model.Board;
 //Repository는 JPA에서 쿼리문을 사용하는 곳? DB와 소통창구 
 public interface BoardRepository extends JpaRepository<Board, Integer>{
 	List<Board> findByTitleContainingOrderById(String keyword ,Pageable pageable );
-	
 	List<Board> findByUsernameContainingOrderById(String keyword ,Pageable pageable );
-	
 	List<Board> findByUserId(int userId,Pageable pageable);
 	List<Board> findByUserId(int userId);
 	List<Board> findByUsername(String username , Pageable pageable);
