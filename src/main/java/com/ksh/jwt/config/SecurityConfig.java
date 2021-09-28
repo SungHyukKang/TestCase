07f83ee16ce91c08eaf59d061b02656904c7dde2
성혁 강 <95line@naver.com>
2021-09-28 15:40:11 +0900

    .

--- src/main/java/com/ksh/jwt/config/SecurityConfig.java
+++ src/main/java/com/ksh/jwt/config/SecurityConfig.java
@@ -22,54 +22,50 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled=true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 	private final UserRepository userRepository;
 	private final CorsFilter corsFilter;
 	private final PrincipalDetailService principalDetailService;
+
 	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(principalDetailService).passwordEncoder(passwordEncoder());
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//STATELESS -> 세션을 사용하지 않겠다 .
-		.and()
-		.addFilter(corsFilter) //@CrossOrigin(인증 X) , 시큐리티 필터에 등록 안됨(인증 O)
-		.formLogin()
-		.defaultSuccessUrl("/admin")
-		.and()
-		.httpBasic().disable()
-		.addFilter(new JwtAuthenticationFilter(authenticationManager())) //AuthenticationManager
-		.addFilter(new JwtAuthorizationFilter(authenticationManager(),userRepository)) //AuthenticationManager
-		.authorizeRequests()
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)// STATELESS -> 세션을 사용하지 않겠다 .
+				.and().addFilter(corsFilter) // @CrossOrigin(인증 X) , 시큐리티 필터에 등록 안됨(인증 O)
+				.formLogin().defaultSuccessUrl("/admin").and().httpBasic().disable()
+				.addFilter(new JwtAuthenticationFilter(authenticationManager())) // AuthenticationManager
+				.addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository)) // AuthenticationManager
+				.authorizeRequests()
 //		.antMatchers("/join/**","/index/**","/findPw","/findUsername").permitAll()
-		.antMatchers("/api/v1/user/**")
-			.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
-		.antMatchers("/api/v1/manager/**")
-			.access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+				.antMatchers("/api/v1/user/**")
+				.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+				.antMatchers("/api/v1/manager/**").access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
 //		.antMatchers("/**")
 //			.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
-			.antMatchers("/board/**")
-			.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
-			
-			.anyRequest().permitAll();
-		
-			;
-		
+				.antMatchers("/board/**")
+				.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+
+				.anyRequest().permitAll();
+
+		;
+
 	}
 }
--- src/main/java/com/ksh/jwt/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/ksh/jwt/config/jwt/JwtAuthenticationFilter.java
@@ -22,13 +22,15 @@ import com.ksh.jwt.config.auth.PrincipalDetails;
 import com.ksh.jwt.model.User;
 
 import lombok.RequiredArgsConstructor;
+
 //스프링 시큐리티에서 UsernamePasswordAuthenticationFilter 가 있음
 //login 요청해서 username , password 전송하면(post)
 // UsernamePasswordAuthenticationFilter 동작을 함
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 	private final AuthenticationManager authenticationManager;
-	//login 요청을 하면 로그인 시도를 위해서 실행되는 함수
+
+	// login 요청을 하면 로그인 시도를 위해서 실행되는 함수
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
 			throws AuthenticationException {
@@ -42,67 +44,67 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 //				System.out.println(input);
 //			}
 			ObjectMapper om = new ObjectMapper();
-			User user = om.readValue(request.getInputStream(),User.class);
+			User user = om.readValue(request.getInputStream(), User.class);
 			System.out.println(user);
-			UsernamePasswordAuthenticationToken authenticationToken=
-					new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
-			//PrincipalDetailsService의 loadUserByUsername() 함수가 실행된 후 정상이면 authentication이 리턴됨
-			//DB에 있는 username과 password가 일치한다.
+			UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+					user.getUsername(), user.getPassword());
+			// PrincipalDetailsService의 loadUserByUsername() 함수가 실행된 후 정상이면 authentication이
+			// 리턴됨
+			// DB에 있는 username과 password가 일치한다.
 			System.out.println(authenticationToken);
-			Authentication authentication=
-					authenticationManager.authenticate(authenticationToken);
-			//authenticate() 함수가 호출되면 인증 프로바이더가 UserDetailService의 loadUserByUsername(토큰의 첫번째 파라미터)을 호출하고
-			//UserDetails를 리턴 받아서 토큰의 두번째 파라미터(credential)과 UserDetails(DB값)의 getPassword()와 비교해서 동일하면
-			//Authentication 객체를 만들어서 필터체인으로 리턴해준다.
-			//결론 : 인증 프로바이더에게 알려 줄 필요 없음.
-			//authentication 객체를 session 영역에 저장 해야하고 그 방법이 return 해주면 됨
-			//리턴의 이유는 권한 관리를 security가 대신 해주기 때문에 편하려고 하는거임
-			//굳이 JWT 토큰을 사용하면서 세션을 만들 이유가 없음.근데 단지 권한 처리때문에 session에 넣어준다.
+			Authentication authentication = authenticationManager.authenticate(authenticationToken);
+			// authenticate() 함수가 호출되면 인증 프로바이더가 UserDetailService의 loadUserByUsername(토큰의
+			// 첫번째 파라미터)을 호출하고
+			// UserDetails를 리턴 받아서 토큰의 두번째 파라미터(credential)과 UserDetails(DB값)의
+			// getPassword()와 비교해서 동일하면
+			// Authentication 객체를 만들어서 필터체인으로 리턴해준다.
+			// 결론 : 인증 프로바이더에게 알려 줄 필요 없음.
+			// authentication 객체를 session 영역에 저장 해야하고 그 방법이 return 해주면 됨
+			// 리턴의 이유는 권한 관리를 security가 대신 해주기 때문에 편하려고 하는거임
+			// 굳이 JWT 토큰을 사용하면서 세션을 만들 이유가 없음.근데 단지 권한 처리때문에 session에 넣어준다.
 			//
-			PrincipalDetails principalDetails = (PrincipalDetails)authentication.getPrincipal();
-			System.out.println("로그인 완료 :"+principalDetails.getUser().getUsername());
+			PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+			System.out.println("로그인 완료 :" + principalDetails.getUser().getUsername());
 			System.out.println(authentication);
 			return authentication;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
+
 	// attemptAuthentication 실행 후 인증이 정상적으로 되었으면 successfulAuthenticaton 함수가 실행된다.
-	//JWT 토큰을 만들어서 request요청한 사용자에게 JWT 토큰을 response해주면 됨
+	// JWT 토큰을 만들어서 request요청한 사용자에게 JWT 토큰을 response해주면 됨
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 			Authentication authResult) throws IOException, ServletException {
 		System.out.println("successfultAuthenticat 실행됨: 인증 완료");
-		PrincipalDetails principalDetails = (PrincipalDetails)authResult.getPrincipal();
-		//RSA방식은 아니고 , Hash암호 방식
+		PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+		// RSA방식은 아니고 , Hash암호 방식
 		PrintWriter out = response.getWriter();
-		String jwtToken = JWT.create()
-				.withSubject("토큰")
-				.withExpiresAt(new Date(System.currentTimeMillis()+JwtProperties.EXPIRATION_TIME))
+		String jwtToken = JWT.create().withSubject("토큰")
+				.withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
 				.withClaim("id", principalDetails.getUser().getId())
 				.withClaim("username", principalDetails.getUser().getUsername())
 				.sign(Algorithm.HMAC512(JwtProperties.SECRET));
-		
-		String refToken = JWT.create()
-				.withSubject("토큰")
-				.withExpiresAt(new Date(System.currentTimeMillis()+JwtProperties.EXPIRATION_TIME*1000))
+
+		String refToken = JWT.create().withSubject("토큰")
+				.withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME * 1000))
 				.withClaim("id", principalDetails.getUser().getId())
 				.withClaim("username", principalDetails.getUser().getUsername())
 				.sign(Algorithm.HMAC512(JwtProperties.SECRET));
-		
-		response.addHeader(JwtProperties.HEADER_STRING,JwtProperties.TOKEN_PREFIX+jwtToken);
-		response.addHeader(JwtProperties.REFRESH_STRING,JwtProperties.TOKEN_PREFIX+refToken);
+
+		response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
+		response.addHeader(JwtProperties.REFRESH_STRING, JwtProperties.TOKEN_PREFIX + refToken);
 		out.print("{\n");
-		out.print("\"accessToken\""+":"+" \""+jwtToken+"\"");
-		out.print(",\n\"refreshToken\""+":"+" \""+refToken+"\"");
-		out.print(",\n\"expireIn\""+":"+" \""+"600000"+"\"");
+		out.print("\"accessToken\"" + ":" + " \"" + jwtToken + "\"");
+		out.print(",\n\"refreshToken\"" + ":" + " \"" + refToken + "\"");
+		out.print(",\n\"expireIn\"" + ":" + " \"" + "600000" + "\"");
 		out.print("\n}");
 		out.flush();
 		out.close();
 	}
-	
+
 }
